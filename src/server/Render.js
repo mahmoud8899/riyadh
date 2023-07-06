56d531c9ff7478fadd39fd261116b84f4d589c0b
mahmoud.talat899@gmail.com <mahmoud.talat899@gmail.com>
2023-07-06 23:05:48 +0200

    add google tag

--- src/server/Render.js
+++ src/server/Render.js
@@ -35,6 +35,14 @@ ${helmet.link.toString()}
       <meta name="theme-color" content="#d8bc64" />
     <link rel="stylesheet" type="text/css" href="/css/main.css" />
 
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N92R4JS');</script>
+<!-- End Google Tag Manager -->
+
 
 
 
@@ -45,7 +53,10 @@ ${helmet.link.toString()}
          ${appString}
     </div>
     <script src="/client/bundle.js" defer ></script>
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N92R4JS"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     
 </body>
 </html>
